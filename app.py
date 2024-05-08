262a8b825213a007bf83449c5addbbd8704bd398
zeeza_18 <mdazeezulla2001@gmail.com>
2024-05-08 00:26:53 -0500

    port changes

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application = Flask(__name__)
-app = application
+application=Flask(__name__)
+
+app=application
+
+## Route for a home page
 
 @app.route('/')
 def index():
@@ -24,20 +28,18 @@ def predict_datapoint():
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-
-        )
+            writing_score=float(request.form.get('reading_score')))
+        
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
-        print("Before Prediction")
 
         predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
         return render_template('home.html',results=results[0])
+    
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
+    # app.run(host="0.0.0.0",port=8080)        
+    app.run(host='0.0.0.0', port=8080)        
 
